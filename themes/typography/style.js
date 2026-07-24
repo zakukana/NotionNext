1c78193b18b954eb583e740c61a422a7a6ddad87
zakukana <asdfgkz90@gmail.com>
2026-07-24 22:33:54 +0800

    Update style.js

--- themes/typography/style.js
+++ themes/typography/style.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-unknown-property */
 import CONFIG from './config'
 import { themeConsoleStyle } from '@/lib/themeConsoleStyle'
+<link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet" />
 /**
  * 此处样式只对当前主题生效
  * 此处不支持 tailwindCSS 的 @apply 语法
